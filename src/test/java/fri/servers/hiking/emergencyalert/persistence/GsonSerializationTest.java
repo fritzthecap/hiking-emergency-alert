591cd55042bff76e8618b82062115ba69699a3f0
fridell <fritz.ritzberger@chello.at>
2026-01-19 22:34:19 +0100

    Added test for custom mail properties.

--- src/test/java/fri/servers/hiking/emergencyalert/persistence/GsonSerializationTest.java
+++ src/test/java/fri/servers/hiking/emergencyalert/persistence/GsonSerializationTest.java
@@ -2,6 +2,8 @@ package fri.servers.hiking.emergencyalert.persistence;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 /** Test for the library "Gson". */
@@ -28,5 +30,29 @@ class GsonSerializationTest extends AbstractJsonSerializationTest
         // every Hike must have its own UUID
         assertNotEquals(uniqueMailId, hikeFromJson.uniqueMailId);
         assertTestHike(hike, hikeFromJson);
+    }
+    
+    @Test
+    void customMailPropertiesShouldWork() throws IOException {
+        final Hike hike = newHike();
+        final MailConfiguration mailConfiguration = hike.getAlert().getMailConfiguration();
+        final List<List<String>> customProperties = mailConfiguration.getCustomProperties();
+        customProperties.add(buildCustomMailProperty("mail.smtp.auth", "true"));
+        customProperties.add(buildCustomMailProperty("mail.smtp.timeout", "10000"));
+        
+        final JsonGsonSerializer<Hike> serializer = new JsonGsonSerializer<>();
+        final String jsonHike = serializer.toJson(hike);
+        
+        System.out.println(jsonHike);
+        
+        final Hike hikeFromJson = serializer.fromJson(jsonHike, Hike.class);
+        assertTestHike(hike, hikeFromJson);
+    }
+
+    private List<String> buildCustomMailProperty(String name, String value) {
+        final List<String> tuple = new ArrayList<>(2);
+        tuple.add(name);
+        tuple.add(value);
+        return tuple;
     }
 }
