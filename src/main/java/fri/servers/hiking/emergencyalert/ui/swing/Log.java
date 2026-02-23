702bd39b733a50d25a04bb1d5e5ff94f9a4d6096
fridell <fritz.ritzberger@chello.at>
2026-02-23 16:56:33 +0100

    Fixed missing event-queue detection.

--- src/main/java/fri/servers/hiking/emergencyalert/ui/swing/Log.java
+++ src/main/java/fri/servers/hiking/emergencyalert/ui/swing/Log.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 /**
  * Redirect System.out and System.err to some UI window.
@@ -21,7 +22,11 @@ public final class Log
         @Override
         public void flush() throws IOException {
             final String line = toString();
-            textArea.append(line);
+            if (SwingUtilities.isEventDispatchThread())
+                textArea.append(line);
+            else
+                SwingUtilities.invokeLater(() -> textArea.append(line));
+            
             reset(); // else toString() would give all output again next time
         }
     }
@@ -36,4 +41,37 @@ public final class Log
     }
     
     private Log() {} // do not instantiate
+    
+    
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(() -> {
+//            JButton stdoutButton = new JButton("Write to System.out");
+//            stdoutButton.addActionListener(event -> System.out.println("Hello System.out at "+new Date()));
+//            
+//            JButton stderrButton = new JButton("Write to System.err");
+//            stderrButton.addActionListener(event -> System.err.println("Hello System.err at "+new Date()));
+//
+//            JToolBar toolbar = new JToolBar();
+//            toolbar.add(stdoutButton);
+//            toolbar.add(stderrButton);
+//            
+//            JTextArea textArea = new JTextArea();
+//            textArea.setLineWrap(true);
+//            textArea.setWrapStyleWord(true);
+//            
+//            Log.redirectOut(textArea);
+//            Log.redirectErr(textArea);
+//            
+//            JPanel panel = new JPanel(new BorderLayout());
+//            panel.add(new JScrollPane(textArea), BorderLayout.CENTER);
+//            panel.add(toolbar, BorderLayout.NORTH);
+//            
+//            JFrame frame = new JFrame();
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            frame.getContentPane().add(panel);
+//            frame.setSize(new Dimension(400, 200));
+//            frame.setLocationRelativeTo(null);
+//            frame.setVisible(true);
+//        });
+//    }
 }
